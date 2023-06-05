fba085a90e54847c00bf6fc58206936b212c411b
Agus Syahputra <agsyah1508@gmail.com>
2023-06-05 22:01:29 +0700

    Added DotMatrix device output

--- dotmatrix_iot_v1/dotmatrix_iot_v1.ino
+++ dotmatrix_iot_v1/dotmatrix_iot_v1.ino
@@ -4,7 +4,7 @@
     PLAN:
       * Receive text data from MIT
       * Store data for dotmatrix
-      * 
+      * Display DotMatrix
     
     HOW TO USE:
       * Configure your wifi ssid and password
@@ -14,6 +14,22 @@
 
 #include <WiFi.h>                                 // ESP32 Library
 #include <PubSubClient.h>                         // MQTT Library
+
+#include <MD_Parola.h>
+#include <MD_MAX72xx.h>
+#include <SPI.h>
+
+
+// DotMatrix
+#define HARDWARE_TYPE MD_MAX72XX::FC16_HW
+
+#define MAX_DEVICES   8
+#define DATA_PIN      23
+#define CS_PIN        5
+#define CLK_PIN       18
+
+MD_Parola myDisplay = MD_Parola(HARDWARE_TYPE, DATA_PIN, CLK_PIN, CS_PIN, MAX_DEVICES);
+
 
 // WiFi Configuration
 const char* ssid          = "ssid";              // SSID WiFi
@@ -42,12 +58,9 @@ void WiFi_Setup()
   delay(10);
 
   Serial.println("Connecting to WiFi ...");
+
+  WiFi.mode(WIFI_AP);
   WiFi.begin(ssid, password);
-  delay(1000);
-  WiFi.disconnect();
-  delay(1000);
-  WiFi.begin(ssid, password);
-  delay(1000);
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -78,6 +91,14 @@ void MQTT_Setup()
   }
 }
 
+void DotMatrix_Setup()
+{
+    myDisplay.begin();
+    myDisplay.setIntensity(0);
+    myDisplay.displayClear();
+    myDisplay.setTextAlignment(PA_CENTER);
+}
+
 void callback(char* topic, byte* payload, unsigned int length)
 {
   Serial.print("Message from topic: ");
@@ -98,6 +119,7 @@ void callback(char* topic, byte* payload, unsigned int length)
     Serial.println(dotmatrix_text);
 
     // Add your dotmatrix update code here...
+    myDisplay.print(dotmatrix_text);
     
   }
   
